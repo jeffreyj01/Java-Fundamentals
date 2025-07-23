164cf5999ba9d73a7e02b96bc061c88e9098e03d
jeffreyj01 <jeffreyj0112@gmail.com>
2025-07-23 14:59:11 -0600

    Arrays and Recursion Practice

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,68 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        int month = 5;
-
+        //int month = 5;
         //Call Class Method to practice switch statement
-        System.out.println(monthJoined(month));
+        //System.out.println(monthJoined(month));
+
+        //Array Practice
+        int[] numbersOneToFive = {1, 2, 3, 4, 5 };
+        for(int i : numbersOneToFive)
+        {
+            System.out.println(i);
         }
+
+        System.out.println("--------------------------------");
+        System.out.println();
+
+        //Two Dimensional Array
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        for (int[] row : myNumbers) {
+            for (int i : row) {
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("--------------------------------");
+        System.out.println();
+
+        //Should print 3
+        System.out.println(myNumbers[0][2]);
+
+        System.out.println("--------------------------------");
+        System.out.println();
+
+        //Use recursion to add all of the numbers up to 10.
+        int result = sum(10);
+        System.out.println(result);
+
+        System.out.println("--------------------------------");
+        System.out.println();
+
+        //Use recursion to add all of the numbers between 5 to 10.
+        result = sum(5, 10);
+        System.out.println(result);
+
+    }
+
+    private static int sum(int n) {
+        if (n > 0)
+        {
+            return n + sum(n - 1);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public static int sum(int start, int end) {
+        if (end > start) {
+            return end + sum(start, end - 1);
+        } else {
+            return end;
+        }
+    }
 
         private static String monthJoined(int month) {
 
